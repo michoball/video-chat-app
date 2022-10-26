a6404fdde2249874e4975221769a4cee876bb7bf
michoball <myunghun0114@gmail.com>
2022-10-26 22:19:58 +0900

    refactor: change rtm action and reducer js to ts

--- src/store/rtm/rtm.type.ts
+++ src/store/rtm/rtm.type.ts
@@ -2,6 +2,14 @@ export enum RTM_ACTION_TYPE {
   SET_RTM_CLIENT = "SET_RTM_CLIENT",
   ADD_NEW_MESSAGE = "ADD_NEW_MESSAGE",
   SET_RTM_USER = "SET_RTM_USER",
-  CLEAR_ALL = "CLEAR_ALL",
+  CLEAR_RTM = "CLEAR_RTM",
   SET_CHANNEL = "SET_CHANNEL",
 }
+
+export type Messages = {
+  id: string;
+  message: string;
+  from: string;
+  type: string;
+  displayName: string;
+};
