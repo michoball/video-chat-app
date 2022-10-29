758fe0680a89e3c4f0585efcf393bd8e79e31c3b
michoball <myunghun0114@gmail.com>
2022-10-29 16:49:51 +0900

    refactor: change UI components to typescript

---
+++ src/UI/formInput/FormInput.tsx
@@ -0,0 +1,18 @@
+import React, { InputHTMLAttributes } from "react";
+import { Input, FormInputContainer, FromInputLabel } from "./FormInput.styles";
+
+type FromInputProps = { label: string } & InputHTMLAttributes<HTMLInputElement>;
+
+const FormInput = React.forwardRef<HTMLInputElement, FromInputProps>(
+  (props, ref) => {
+    const { label, ...otherProps } = props;
+    return (
+      <FormInputContainer>
+        <FromInputLabel htmlFor={label}>{label && label}</FromInputLabel>
+        <Input ref={ref} {...otherProps} />
+      </FormInputContainer>
+    );
+  }
+);
+
+export default FormInput;
