828d67c3d05c23e2cefbbbc3d423635735b5b4ee
michoball <myunghun0114@gmail.com>
2022-10-31 19:24:26 +0900

    fix: fix import error

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
-import { ColorProvider } from "./context/color.context";
+import ColorProvider from "./context/color.context";
 
 ReactDOM.render(
   <React.StrictMode>
