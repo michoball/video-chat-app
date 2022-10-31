00d3eabf82c421f101601dc354ef29dce72edc72
michoball <myunghun0114@gmail.com>
2022-10-31 19:20:30 +0900

    refactor: change privateRoute components js file to ts

---
+++ src/components/privateRoute/PrivateRoute.tsx
@@ -0,0 +1,16 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+const PrivateRoute = () => {
+  let userStorage = localStorage.getItem("user");
+  if (userStorage) {
+    const currentUser = JSON.parse(userStorage);
+
+    if (!currentUser) {
+      alert("Log in First");
+      return <Navigate to="/auth" />;
+    }
+  }
+  return <Outlet />;
+};
+
+export default PrivateRoute;
